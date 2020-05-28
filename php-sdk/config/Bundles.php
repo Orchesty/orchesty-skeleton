b5305cf1938b28bcfb9d231e15de394356a33478
Radek Jirsa <jirsa.r@hanaboso.com>
2020-05-28 18:50:24 +0200

    Added AppStore

--- php-sdk/config/Bundles.php
+++ php-sdk/config/Bundles.php
@@ -3,6 +3,7 @@
 use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle;
 use Hanaboso\CommonsBundle\HbPFCommonsBundle;
+use Hanaboso\HbPFAppStore\HbPFAppStoreBundle;
 use Hanaboso\PipesPhpSdk\HbPFApplicationBundle\HbPFApplicationBundle;
 use Hanaboso\PipesPhpSdk\HbPFConnectorBundle\HbPFConnectorBundle;
 use Hanaboso\PipesPhpSdk\HbPFCustomNodeBundle\HbPFCustomNodeBundle;
@@ -24,6 +25,7 @@ return [
     SecurityBundle::class        => ['all' => TRUE],
 
     HbPFApplicationBundle::class     => ['all' => TRUE],
+    HbPFAppStoreBundle::class        => ['all' => TRUE],
     HbPFCommonsBundle::class         => ['all' => TRUE],
     HbPFConnectorBundle::class       => ['all' => TRUE],
     HbPFCustomNodeBundle::class      => ['all' => TRUE],
