711e319da906bc6364d272e035ded33920ac08fa
Radek Jirsa <jirsa.r@ghanaboso.com>
2021-10-13 16:50:36 +0200

    Packages updated

--- node-sdk/.jest/testEnvs.ts
+++ node-sdk/.jest/testEnvs.ts
@@ -5,13 +5,11 @@ process.env.BACKEND_URL = 'http://127.0.0.42:8080'
 
 if (process.env.JEST_DOCKER) {
   // --- DOCKER ---
-  process.env.REDIS_HOST = 'redis'
   process.env.UDP_LOGGER_DSN = 'logstash:5005'
   process.env.METRICS_DSN = 'mongodb://mongo:27017/metrics'
   process.env.MONGO_DSN = 'mongodb://mongo:27017/node-sdk'
 } else {
   // --- LOCALHOST ---
-  process.env.REDIS_HOST = '127.0.0.42'
   process.env.UDP_LOGGER_DSN = '127.0.0.42:5005'
   process.env.METRICS_DSN = 'mongodb://127.0.0.42:27017/metrics'
   process.env.MONGO_DSN = 'mongodb://127.0.0.42:27017/node-sdk'
@@ -22,6 +20,11 @@ jest.mock('pipes-nodejs-sdk/dist/lib/Logger/Logger', () => ({
   error: () => jest.fn(),
   info: () => jest.fn(),
   debug: () => jest.fn(),
+  log: () => jest.fn(),
+  ctxFromDto: () => jest.fn(),
+  ctxFromReq: () => jest.fn(),
   // eslint-disable-next-line @typescript-eslint/naming-convention
   Logger: jest.fn().mockImplementation(() => ({})),
 }));
+
+jest.setTimeout(10000);
--- node-sdk/jest.config.js
+++ node-sdk/jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/__tests__/*.ts'],
-  setupFiles: ["<rootDir>/.jest/testEnvs.js"],
+  setupFiles: ["<rootDir>/.jest/testEnvs.ts"],
+  setupFilesAfterEnv: ["<rootDir>/.jest/testLifecycle.ts"],
 };
