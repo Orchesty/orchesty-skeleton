a04619c58fe6285ebea3392287d45a3cd4a40dcc
Radek Jirsa <jirsa.r@ghanaboso.com>
2022-10-21 10:09:17 +0200

    Updated index.ts

--- nodejs-sdk/src/index.ts
+++ nodejs-sdk/src/index.ts
@@ -4,6 +4,19 @@ async function prepare(): Promise<void> {
 // Load core services by:
     await initiateContainer();
 
+    // System event services - imported from @orchesty/nodejs-connectors
+    // const eventStatusFilterSuccess = new EventStatusFilter(EventEnum.PROCESS_SUCCESS);
+    // container.setCustomNode(eventStatusFilterSuccess);
+
+    // const eventStatusFilterError = new EventStatusFilter(EventEnum.PROCESS_FAILED);
+    // container.setCustomNode(eventStatusFilterError);
+
+    // const eventStatusFilterLimiter = new EventStatusFilter(EventEnum.LIMIT_OVERFLOW);
+    // container.setCustomNode(eventStatusFilterLimiter);
+
+    // const eventStatusFilterTrash = new EventStatusFilter(EventEnum.MESSAGE_IN_TRASH);
+    // container.setCustomNode(eventStatusFilterTrash);
+
     // Express.js is available by import:
     // import { expressApp } from '@orchesty/nodejs-sdk';
 
